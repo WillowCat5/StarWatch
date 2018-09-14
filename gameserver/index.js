85aa27371e1c0394724cf3157bad8618a7eeaca0
Bryan Scott <bscott@scottclan.org>
2018-09-14 10:28:55 -0600

    Hooked up Socket.io and added random colors per player

--- gameserver/index.js
+++ gameserver/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 function onConnection(socket) {
     console.log('Connection received: ' + socket.request.connection.remoteAddress + ":" + socket.request.connection.remotePort);
     socket.on('move', (data) => {
-	    socket.broadcast.emit('move', data)
+	    socket.broadcast.emit('moved', data)
 	    console.log('move received: ');
 	    console.log(data);
 	});
