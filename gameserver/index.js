4b7133a49b6f27f66ae6a40efe4cc45a887a8804
Bryan Scott <bscott@scottclan.org>
2018-09-17 18:02:22 -0600

    Show all users upon connection and remove disconnected users

--- gameserver/index.js
+++ gameserver/index.js
@@ -3,12 +3,18 @@ const io = require('socket.io')();
 const port = process.env.PORT || 3000;
 
 function onConnection(socket) {
-    console.log('Connection received: ' + socket.request.connection.remoteAddress + ":" + socket.request.connection.remotePort);
+	//console.log('Connection received: ' + socket.request.connection.remoteAddress + ":" + socket.request.connection.remotePort);
+	socket.broadcast.emit('announce', {} )
+
     socket.on('move', (data) => {
-	    socket.broadcast.emit('moved', data)
-	    console.log('move received: ');
-	    console.log(data);
+		socket.broadcast.emit('moved', data)
+		socket.userid = data.uuid;
+	    //console.log('move received: ');
+	    //console.log(data);
 	});
+	socket.on('disconnect', () => {
+		socket.broadcast.emit('remove', { "uuid" : socket.userid })
+	})
 }
 
 io.on('connection', onConnection);
